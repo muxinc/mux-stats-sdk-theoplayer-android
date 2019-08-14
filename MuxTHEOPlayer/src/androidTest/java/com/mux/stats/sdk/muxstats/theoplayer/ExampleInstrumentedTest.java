beb488237f5890440b5ecba50035465cc390a761
Justin Sanford <justin.h.sanford@gmail.com>
2019-08-14 16:41:16 -0700

    one more

--- MuxTHEOPlayer/src/androidTest/java/com/mux/stats/sdk/muxstats/theoplayer/ExampleInstrumentedTest.java
+++ MuxTHEOPlayer/src/androidTest/java/com/mux/stats/sdk/muxstats/theoplayer/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.mux.stats.sdk.muxstats.theoplayer.theoplayer.test", appContext.getPackageName());
+        assertEquals("com.mux.stats.sdk.muxstats.theoplayer.test", appContext.getPackageName());
     }
 }
