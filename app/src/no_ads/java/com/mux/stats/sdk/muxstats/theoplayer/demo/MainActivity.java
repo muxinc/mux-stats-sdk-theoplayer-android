9fae48456a4ceaa3dfbc754061a6ea3f47482fad
Tomislav Kordic <tkordic.private@gmail.com>
2020-03-27 00:53:01 +0100

    Updated the build system, using androidx

--- app/src/no_ads/java/com/mux/stats/sdk/muxstats/theoplayer/demo/MainActivity.java
+++ app/src/no_ads/java/com/mux/stats/sdk/muxstats/theoplayer/demo/MainActivity.java
@@ -2,11 +2,14 @@ package com.mux.stats.sdk.muxstats.theoplayer.demo;
 
 import android.graphics.Point;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.appbar.MaterialToolbar;
 import com.mux.stats.sdk.core.model.CustomerPlayerData;
 import com.mux.stats.sdk.core.model.CustomerVideoData;
 import com.mux.stats.sdk.muxstats.theoplayer.MuxStatsSDKTHEOplayer;
@@ -16,16 +19,22 @@ import com.theoplayer.android.api.event.player.PauseEvent;
 import com.theoplayer.android.api.event.player.PlayEvent;
 import com.theoplayer.android.api.event.player.PlayerEventTypes;
 import com.theoplayer.android.api.event.player.TimeUpdateEvent;
+import com.theoplayer.android.api.player.Player;
 import com.theoplayer.android.api.source.SourceDescription;
 import com.theoplayer.android.api.source.SourceType;
 import com.theoplayer.android.api.source.TypedSource;
 
+import static com.theoplayer.android.api.source.SourceDescription.Builder.sourceDescription;
+import static com.theoplayer.android.api.source.TypedSource.Builder.typedSource;
+
 public class MainActivity extends AppCompatActivity {
 
+    static final String TAG = "MainActivity";
+
     THEOplayerView theoPlayerView;
+    private Player theoPlayer;
     Button btnPlayPause;
     TextView txtPlayStatus, txtTimeUpdate;
-
     MuxStatsSDKTHEOplayer muxStatsSDKTHEOplayer;
 
     @Override
@@ -34,21 +43,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         theoPlayerView = findViewById(R.id.theoplayer);
-        theoPlayerView.getSettings().setFullScreenOrientationCoupled(true);
+        theoPlayer = theoPlayerView.getPlayer();
 
+        // Configuring action bar.
+//        setSupportActionBar(findViewById(R.id.toolbar));
 
-        TypedSource typedSource = TypedSource.Builder
-                .typedSource()
-                .src("https://cdn.theoplayer.com/video/dash/big_buck_bunny/BigBuckBunny_10s_simple_2014_05_09.mpd")
-                .type(SourceType.DASH)
-                .build();
-
-        SourceDescription sourceDescription = SourceDescription.Builder
-                .sourceDescription(typedSource)
-                .build();
-
-        theoPlayerView.getPlayer().setSource(sourceDescription);
-
+        configureTHEOplayer();
         btnPlayPause = findViewById(R.id.btn_playpause);
         btnPlayPause.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,40 +60,62 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
+//
         txtPlayStatus = findViewById(R.id.txt_playstatus);
         txtTimeUpdate = findViewById(R.id.txt_timeupdate);
 
-        theoPlayerView.getPlayer().addEventListener(PlayerEventTypes.PLAY, new EventListener<PlayEvent>() {
-            @Override
-            public void handleEvent(PlayEvent playEvent) {
-                txtPlayStatus.setText("Playing");
-            }
-        });
+        configureMuxSdk();
+    }
 
-        theoPlayerView.getPlayer().addEventListener(PlayerEventTypes.PAUSE, new EventListener<PauseEvent>() {
-            @Override
-            public void handleEvent(PauseEvent pauseEvent) {
-                txtPlayStatus.setText("Paused");
-            }
-        });
-
-        theoPlayerView.getPlayer().addEventListener(PlayerEventTypes.TIMEUPDATE, new EventListener<TimeUpdateEvent>() {
-            @Override
-            public void handleEvent(TimeUpdateEvent timeUpdateEvent) {
-                txtTimeUpdate.setText(String.valueOf(timeUpdateEvent.getCurrentTime()));
-            }
-        });
-
+    private void configureMuxSdk() {
         CustomerPlayerData customerPlayerData = new CustomerPlayerData();
         customerPlayerData.setEnvironmentKey("g48011fena4sr1k35jck5eokt");
         CustomerVideoData customerVideoData = new CustomerVideoData();
-        customerVideoData.setVideoTitle("Big Buck");
+        customerVideoData.setVideoTitle("Big buck bunny");
         muxStatsSDKTHEOplayer = new MuxStatsSDKTHEOplayer(this, theoPlayerView, "demo-view-player", customerPlayerData, customerVideoData);
 
         Point size = new Point();
         getWindowManager().getDefaultDisplay().getSize(size);
         muxStatsSDKTHEOplayer.setScreenSize(size.x, size.y);
+    }
+
+    private void configureTHEOplayer() {
+        // Coupling the orientation of the device with the fullscreen state.
+        // The player will go fullscreen when the device is rotated to landscape
+        // and will also exit fullscreen when the device is rotated back to portrait.
+        theoPlayerView.getSettings().setFullScreenOrientationCoupled(true);
+
+        // Creating a TypedSource builder that defines the location of a single stream source.
+        TypedSource.Builder typedSource =  typedSource(getString(R.string.defaultSourceUrl));
+
+        // Creating a SourceDescription builder that contains the settings to be applied as a new
+        // THEOplayer source.
+        SourceDescription.Builder sourceDescription = sourceDescription(typedSource.build());
+        // Skip the default poster
+//                .poster(getString(R.string.defaultPosterUrl));
+
+        // Configuring THEOplayer with defined SourceDescription object.
+        theoPlayer.setSource(sourceDescription.build());
+
+        theoPlayer.addEventListener(PlayerEventTypes.PLAYING, event ->
+                txtPlayStatus.setText("Playing")
+        );
+
+        theoPlayer.addEventListener(PlayerEventTypes.PAUSE, event ->
+                txtPlayStatus.setText("Paused")
+        );
+
+        theoPlayer.addEventListener(PlayerEventTypes.ENDED, event ->
+                txtPlayStatus.setText("Ended")
+        );
+
+        theoPlayer.addEventListener(PlayerEventTypes.ERROR, event ->
+                txtPlayStatus.setText("Error: " + event.getError())
+        );
+
+        theoPlayer.addEventListener(PlayerEventTypes.TIMEUPDATE, event ->
+                txtTimeUpdate.setText(String.valueOf(event.getCurrentTime()))
+        );
     }
 
     @Override
