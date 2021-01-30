61e6e0a1ce0fe60ddaa96172c8497dee5db78f02
Tomislav Kordic <tkordic.private@gmail.com>
2021-01-30 20:33:14 -0300

    Updated to Androidx.

--- app/src/main/java/com/mux/stats/sdk/muxstats/theoplayer/MainActivity.java
+++ app/src/main/java/com/mux/stats/sdk/muxstats/theoplayer/MainActivity.java
@@ -2,7 +2,7 @@ package com.mux.stats.sdk.muxstats.theoplayer;
 
 import android.graphics.Point;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
